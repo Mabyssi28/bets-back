f2ff65f07ebb2a2374eb272ebc678915fe5a324b
Josué Rivera Henao <josu.riverah@gmail.com>
2022-04-15 14:58:17 -0500

    Init server

---
+++ src/app.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const app = express();
+const port = 3000 || process.env.port;
+
+app.get('/', (req, res) => {
+    res.send('Hello');
+});
+
+app.listen(port, ()=> console.log(`Listening on port ${port}!`));
